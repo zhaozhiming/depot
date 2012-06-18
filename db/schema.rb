d977e62a0d2c40105157cd31ba9dcdd770ab04e1
Zhao Zhiming <kingzzm1982@sina.com>
2012-06-19 07:54:57 +0800

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120617004440) do
+ActiveRecord::Schema.define(:version => 20120618145501) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -49,6 +49,14 @@ ActiveRecord::Schema.define(:version => 20120617004440) do
     t.decimal  "price",       :precision => 8, :scale => 2
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "hashed_password"
+    t.string   "salt"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
